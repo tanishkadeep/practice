e480d0103c7a86ead95409774571c7447d55098f
Tanishka <tanishkadeep09@gmail.com>
2024-08-29 23:12:15 +0530

    useLocation

--- form-app/src/pages/Display.tsx
+++ form-app/src/pages/Display.tsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 const Display = () => {
-  return <div></div>;
+  const location = useLocation();
+  const { name, age } = location.state;
+
+  useEffect(() => {
+    console.log(location);
+  }, []);
+
+  return (
+    <div>
+      <div>Name : {name}</div>
+      <div>Age : {age}</div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Display;
