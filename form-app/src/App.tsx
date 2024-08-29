2f00a6e9f368abe7151bdba0dfa693307a2695d1
Tanishka <tanishkadeep09@gmail.com>
2024-08-29 22:44:26 +0530

    form

--- form-app/src/App.tsx
+++ form-app/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChangeEvent, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [form, setForm] = useState({});
+
+  function handleSubmit() {}
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    return setForm({ ...form, [e.target.name]: e.target.value });
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label> Name: </label>
+        <input type="text" name="name" onChange={handleChange} />
+        <label> Age: </label>
+        <input type="number" name="age" onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
